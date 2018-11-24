e49f20c381deb52d413e8a33383e687d3d5697c9
ckokkalis <charalampos.kokkalis@gmail.com>
2018-11-24 12:48:39 +0000

    ck

--- emotion.py
+++ emotion.py
@@ -1,1 +1,71 @@
+# test file to make the emotion thing work - microsoft's walkthrough
 
+# IMPORTS
+
+import requests
+from IPython.display import HTML
+
+import matplotlib as mpl # these two lines fix the matplotlib importing error for macOS
+mpl.use('TkAgg')
+
+import matplotlib.pyplot as plt
+
+from PIL import Image # download pillow instead of PIL, still call import PIL
+from matplotlib import patches
+from io import BytesIO
+
+subscription_key = "10670255bd374b9bb852b35885713b05"
+assert subscription_key
+
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+image_url = 'https://how-old.net/Images/faces2/main007.jpg'
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+}
+
+response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+faces = response.json()
+HTML("<font size=5>Detected <font color='blue'>%d</font> faces in the image</font>" % len(faces))
+
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+plt.figure(figsize=(8,8))
+ax = plt.imshow(image, alpha=0.6)
+for face in faces:
+    fr = face["faceRectangle"]
+    fa = face["faceAttributes"]
+    origin = (fr["left"], fr["top"])
+    p = patches.Rectangle(origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+    ax.axes.add_patch(p)
+    plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]), fontsize=20, weight="bold", va="bottom")
+_ = plt.axis("off")
+
+
+def annotate_image(image_url):
+    response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+    faces = response.json()
+
+    image_file = BytesIO(requests.get(image_url).content)
+    image = Image.open(image_file)
+
+    plt.figure(figsize=(8,8))
+    ax = plt.imshow(image, alpha=0.6)
+    for face in faces:
+        fr = face["faceRectangle"]
+        fa = face["faceAttributes"]
+        origin = (fr["left"], fr["top"])
+        p = patches.Rectangle(origin, fr["width"], \
+                              fr["height"], fill=False, linewidth=2, color='b')
+        ax.axes.add_patch(p)
+        plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]), \
+                 fontsize=20, weight="bold", va="bottom")
+    plt.axis("off")
+
+
+annotate_image("https://how-old.net/Images/faces2/main001.jpg")
