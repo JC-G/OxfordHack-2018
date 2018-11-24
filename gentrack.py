ecea158ccf3d6a291d420fa0d669272c62b12f44
Fan Man Hon <lyndonf@Fans-MacBook-Pro.local>
2018-11-24 12:51:39 +0000

    updated gentrack.py

--- gentrack.py
+++ gentrack.py
@@ -1,9 +1,12 @@
+#adapted from http://blog.meltinglogic.com/2013/12/how-to-generate-procedural-racetracks/
+
 from random import *
 from math import *
 import sys
 from scipy.spatial import *
 import matplotlib.pyplot as plt
 import numpy as np
+from turtle import *
 
 def dist(pta,ptb = [0,0]):
     return sqrt((pta[0]-ptb[0])**2 + (pta[1]-ptb[1])**2)
@@ -11,90 +14,122 @@ def dist(pta,ptb = [0,0]):
 screenx = 500
 screeny = 500
 
-numOfPts = 30
-points = np.random.rand(numOfPts, 2)   # 30 random points in 2-D
-for i in range(numOfPts):
-    points[i][0] = (points[i][0]-0.5)*screenx
-    points[i][1] = (points[i][1]-0.5)*screenx
-hull = ConvexHull(points)
+def run():
+    numOfPts = 30
+    points = np.random.rand(numOfPts, 2)   # 30 random points in 2-D
+    for i in range(numOfPts):
+        points[i][0] = (points[i][0]-0.5)*screenx
+        points[i][1] = (points[i][1]-0.5)*screenx
+    hull = ConvexHull(points)
 
-pts = []
-for i in range(len(points[hull.vertices,0])):
-    pts.append([points[hull.vertices,0][i],points[hull.vertices,1][i]])
-print(pts)
+    pts = []
+    for i in range(len(points[hull.vertices,0])):
+        pts.append([points[hull.vertices,0][i],points[hull.vertices,1][i]])
+    print(pts)
 
-def pushApart(ps):
-    dst = 0.1
-    dst2 = dst**2
-    lth = len(ps)
-    for i in range(0,lth):
-        for j in range(i+1,lth):
-            if dist(ps[i],ps[j]) < dst2:
-                hx = ps[j][0] - ps[i][0]
-                hy = ps[j][1] - ps[i][1]
-                hl = sqrt(hx**2+hy**2)
-                hx /= hl
-                hy /= hl
-                dif = dst - hl
-                hx *= dif
-                hy *= dif
-                ps[j][0] += hx
-                ps[j][1] += hy
-                ps[i][0] -= hx
-                ps[i][1] -= hy
-    return ps
+    def pushApart(ps):
+        dst = 20
+        lth = len(ps)
+        for i in range(0,lth):
+            for j in range(i+1,lth):
+                if dist(ps[i],ps[j]) < dst:
+                    hx = ps[j][0] - ps[i][0]
+                    hy = ps[j][1] - ps[i][1]
+                    hl = dist([hx,hy])
+                    hx /= hl
+                    hy /= hl
+                    dif = dst - hl
+                    hx *= dif
+                    hy *= dif
+                    ps[j][0] -= hx
+                    ps[j][1] -= hy
+                    ps[i][0] += hx
+                    ps[i][1] += hy
+        return ps
 
-iter = 3
-for asdf in range(iter):
-    pts = pushApart(pts)
+    for asdf in range(5):
+        pts = pushApart(pts)
 
-rset = []
-disp = [0,0]
-difficulty = 1
-maxDisp = 0.05
-for i in range(len(pts)):
-    dispLen = (uniform(0,1)**difficulty) * maxDisp
-    theta = uniform(0,2*pi)
-    disp = [dispLen*cos(theta),dispLen*sin(theta)]
-    rset.append(pts[i])
-    mid = [(pts[i][0]+pts[(i+1)%len(pts)][0])/2, (pts[i][1]+pts[(i+1)%len(pts)][1])/2]
-    rset.append([mid[0]+pts[i][0], mid[1]+pts[i][1]])
-pts = rset
+    rset = []
+    disp = [0,0]
+    difficulty = 200
+    maxDisp = 1
+    for i in range(len(pts)):
+        dispLen = (uniform(0,1)**difficulty) * maxDisp
+        theta = uniform(0,2*pi)
+        disp = [dispLen*cos(theta),dispLen*sin(theta)]
+        rset.append(pts[i])
+        mid = [(pts[i][0]+pts[(i+1)%len(pts)][0])/2, (pts[i][1]+pts[(i+1)%len(pts)][1])/2]
+        rset.append([mid[0]+pts[i][0], mid[1]+pts[i][1]])
+    pts = rset
 
-def fixAngles(ps):
-    for i in range(len(ps)):
-        prev = (i-1)%len(ps)
-        next = (i+1)%len(ps)
-        px = ps[i][0] - ps[prev][0]
-        py = ps[i][1] - ps[prev][1]
-        pl = dist([px,py])
-        px /= pl
-        py /= pl
-        nx = ps[i][0] - ps[next][0]
-        ny = ps[i][1] - ps[next][1]
-        nl = dist([nx,ny])
-        nx /= nl
-        ny /= nl
-        a = atan2(px*ny-py*nx,px*nx+py*ny)
-        if abs(a) > 5/9*pi:
-            nA = 100 * a / abs(a) * pi / 180
-            diff = nA - a
-            c = cos(diff)
-            s = sin(diff)
-            newX = nx * c - ny * s
-            newY = nx * s + ny * c
-            newX *= nl
-            newY *= nl
-            ps[next][0] = ps[i][0] + newX
-            ps[next][1] = ps[i][1] + newY
-    return ps
+    def fixAngles(ps):
+        for i in range(len(ps)):
+            prev = (i-1)%len(ps)
+            next = (i+1)%len(ps)
+            px = ps[i][0] - ps[prev][0]
+            py = ps[i][1] - ps[prev][1]
+            pl = dist([px,py])
+            px /= pl
+            py /= pl
+            nx = ps[i][0] - ps[next][0]
+            ny = ps[i][1] - ps[next][1]
+            nx = -nx
+            ny = -ny
+            nl = dist([nx,ny])
+            nx /= nl
+            ny /= nl
+            a = atan2(px*ny-py*nx,px*nx+py*ny)
+            if abs(a) > 5/9*pi:
+                nA = 100 * a / abs(a) * pi / 180
+                diff = nA - a
+                c = cos(diff)
+                s = sin(diff)
+                newX = nx * c - ny * s
+                newY = nx * s + ny * c
+                newX *= nl
+                newY *= nl
+                ps[next][0] = ps[i][0] + newX
+                ps[next][1] = ps[i][1] + newY
+        return ps
 
 
-iter = 3
-for asdf in range(iter):
-    pts = pushApart(pts)
+    iter = 1
+    for asdf in range(iter):
+        pts = fixAngles(pts)
+        pts = pushApart(pts)
 
-plt.plot(points[:,0], points[:,1], 'o')
-plt.plot([item[0] for item in pts+pts[:1]], [item[1] for item in pts+pts[:1]], 'k-o')
-plt.show()
+    def selfIntersects(ps):
+        for i in range(len(ps)-2):
+            pta = ps[i]
+            ptb = ps[i+1]
+            ptm = [(pta[0]+ptb[0])/2, (pta[1]+ptb[1])/2]
+            for j in range(i+2, len(ps)-1):
+                ptc = ps[j]
+                ptd = ps[j+1]
+                ptn = [(ptc[0]+ptd[0])/2, (ptc[1]+ptd[1])/2]
+                ad = dist(pta,ptd)
+                bc = dist(ptb,ptc)
+                mn = dist(ptm,ptn)
+                if mn < ad and mn < bc:
+                    return True
+        return False
 
+    if selfIntersects(pts):
+        pts = run()
+    else:
+        up()
+        # original size of canvas is 500 x 500 -- want it to be 200 x 200
+        for i in range(len(pts)):
+            pts[i][0] /= 2
+            pts[i][1] /= 2
+        goto(pts[0])
+        width(10)
+        down()
+        for item in pts:
+            goto(item)
+        goto(pts[0])
+        wait = sys.stdin.readline()
+        return pts
+
+run()
