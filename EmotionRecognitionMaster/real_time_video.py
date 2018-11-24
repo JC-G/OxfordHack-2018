9b55097eccdff2bf55d9b6e19844f1dd5c9cfe85
JCG <josephcg43@gmail.com>
2018-11-24 20:22:05 +0000

    literally everything v2.0

--- EmotionRecognitionMaster/real_time_video.py
+++ EmotionRecognitionMaster/real_time_video.py
@@ -4,10 +4,18 @@ import imutils
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 from pathlib import Path
 
-
-
+detection_model_path = ""
+emotion_model_path = ""
+face_detection = ""
+emotion_classifier = ""
+camera = 0
+go = True
+value = ((0,0))
+EMOTIONS = ["angry", "disgust", "scared", "happy", "sad", "surprised",
+            "neutral"]
 def toReturn(lst):
 
     if lst.index(max(lst)) == 3:
@@ -18,36 +26,47 @@ def toReturn(lst):
         return -1
 
 
+def destroy():
+
+    camera.release()
+    cv2.destroyAllWindows()
 #feelings_faces = []
 #for index, emotion in enumerate(EMOTIONS):
    # feelings_faces.append(cv2.imread('emojis/' + emotion + '.png', -1))
 
 
-
-def mainbit(addloc = ""):
-    # parameters for loading data and images
+def initControl(addloc = ""):
+    global detection_model_path,emotion_model_path,face_detection,emotion_classifier,camera
     detection_model_path = addloc + "haarcascade_files/haarcascade_frontalface_default.xml"
     emotion_model_path = addloc + 'models/_mini_XCEPTION.102-0.66.hdf5'
 
-
-    # hyper-parameters for bounding boxes shape
-    # loading models
     face_detection = cv2.CascadeClassifier(detection_model_path)
     emotion_classifier = load_model(emotion_model_path, compile=False)
-    EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
-    "neutral"]
-
-    cv2.namedWindow('your_face')
     camera = cv2.VideoCapture(0)
-    # starting video streaming
-    
-    while True:
+
+def captureFrame():
+    initControl("EmotionRecognitionMaster/")
+    global go, value
+    go = True
+    while go:
+
+        time.sleep(0.01)
+        # parameters for loading data and images
+
+
+        # hyper-parameters for bounding boxes shape
+        # loading models
+
+
+        #cv2.namedWindow('your_face')
+        # starting video streaming
+
         frame = camera.read()[1]
         #reading the frame
         frame = imutils.resize(frame,width=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
-        
+
         canvas = np.zeros((500, 600, 3), dtype="uint8")
         frameClone = frame.copy()
         if len(faces) > 0:
@@ -61,8 +80,8 @@ def mainbit(addloc = ""):
             roi = roi.astype("float") / 255.0
             roi = img_to_array(roi)
             roi = np.expand_dims(roi, axis=0)
-            
-            
+
+
             preds = emotion_classifier.predict(roi)[0]
             emotion_probability = np.max(preds)
             label = EMOTIONS[preds.argmax()]
@@ -89,23 +108,19 @@ def mainbit(addloc = ""):
                         elif fX+fW/2 < 600/2 - err/2:
                             acc = (600/2 - err/2 - (fX+fW/2))/(600-err)*20
 
-                        cv2.rectangle(canvas, (7, (i * 35) + 5),
-                        (w, (i * 35) + 35), (0, 0, 255), -1)
-                        cv2.putText(canvas, text, (10, (i * 35) + 23), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
-                        cv2.putText(frameClone, label+" "+str(acc), (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-                        cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
-        cv2.imshow('your_face', frameClone)
-        cv2.imshow("Probabilities", canvas)
+                        # cv2.rectangle(canvas, (7, (i * 35) + 5),
+                        # (w, (i * 35) + 35), (0, 0, 255), -1)
+                        # cv2.putText(canvas, text, (10, (i * 35) + 23), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
+                        # cv2.putText(frameClone, label+" "+str(acc), (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                        # cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
+        #cv2.imshow('your_face', frameClone)
+        #cv2.imshow("Probabilities", canvas)
         if len(faces)>0:
-            yield ((toReturn(resList),acc))
+            value = ((toReturn(resList),acc))
         else:
-            yield ((0,0))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    camera.release()
-    cv2.destroyAllWindows()
+            value = ((0,0))
 
-
+    destroy()
 
 #    for c in range(0, 3):
 #        frame[200:320, 10:130, c] = emoji_face[:, :, c] * \
--- MAIN.py
+++ MAIN.py
@@ -6,31 +6,45 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import math
 import random
+import EmotionRecognitionMaster.real_time_video as rtv
+import threading
 #main loop
 main_loop = True
-util3d.initGLPG((800,600))
+util3d.initGLPG((800, 600))
 theta = 0
-game_state = "playing" #menu, playing, gameover...
+game_state = "playing"  #menu, playing, gameover...
 our_player = Player.Player()
 theTerrain = Map.Terrain()
 clock = pygame.time.Clock()
 trees = []
+
+
+class controlThread(threading.Thread):
+    def __init__(self,name,counter):
+        threading.Thread.__init__(self)
+        self.threadID = counter
+        self.name = name
+        self.counter = counter
+    def run(self):
+        rtv.captureFrame()
+
+theThread = controlThread("a",1)
+theThread.start()
 for x in range(100):
     trees.append(util3d.Sprite3d("tree.png",0.03*random.randint(0,100)-.5,0,0.03*random.randint(0,100)-.5,.1))
 
 
 while main_loop:
     clock.tick(60)
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             main_loop = False
 
     #switch depending on game state
     if game_state == "playing":
+        theVal = rtv.value
 
-        turnMagnitude = 0
+        turnMagnitude = theVal[1]
         forwards = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
@@ -38,13 +52,13 @@ while main_loop:
             forwards = -1
         if keys[pygame.K_s]:
             forwards = 1
-        if keys[pygame.K_a]:
+        # if keys[pygame.K_a]:
+        #
+        #     turnMagnitude = -1
+        # if keys[pygame.K_d]:
+        #     turnMagnitude = 1
 
-            turnMagnitude = -1
-        if keys[pygame.K_d]:
-            turnMagnitude = 1
-
-        print(keys , turnMagnitude)
+        #print(keys , turnMagnitude)
         our_player.move(turnMagnitude*0.1,forwards*0.01)
         glClearColor(0.5, 0.5, 0.5, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -55,7 +69,7 @@ while main_loop:
 
         gluLookAt(our_player.position[0]+math.cos(our_player.theta)/7, .05, our_player.position[1]+math.sin(our_player.theta)/7, our_player.position[0], 0, our_player.position[1], 0, 1,0)
         #glRotate(theta,0,1,0)
-        theta+=1
+        theta+=0.03
         util3d.drawTerrain(theTerrain)
         for t in trees:
             util3d.renderSprite(t)
@@ -70,8 +84,8 @@ while main_loop:
 
 
 
-
-
+rtv.go = False
+theThread.join()
 pygame.quit()
 
 
