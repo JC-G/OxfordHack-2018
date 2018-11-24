aba14c258cac8457d8448983ba275cb8bfcc95e6
JCG <josephcg43@gmail.com>
2018-11-24 15:38:23 +0000

    i

--- Map.py
+++ Map.py
@@ -1,5 +1,8 @@
 import util3d
+import gentrack
+import gentrackdata
 
 class Terrain:
     def __init__(self):
-        self.img = util3d.makeTexture("tree.png")
+
+        self.img = util3d.makeTexture("track.png")
