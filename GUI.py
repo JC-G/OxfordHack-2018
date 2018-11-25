47a983415228106d5cfb0448d734f409e6257014
ckokkalis <charalampos.kokkalis@gmail.com>
2018-11-25 00:39:05 +0000

    This .py file creates the GUI for our app. We need to use the rest of the .py files in the repository as functions for the buttons in this one

--- GUI.py
+++ GUI.py
@@ -1,52 +1,111 @@
-import kivy
 from kivy.app import App
 from kivy.lang import Builder
 
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-
-from kivy.properties import StringProperty
-from kivy.properties import BooleanProperty
-
-
-
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 
 Builder.load_string('''
 <Menu>:
+    orientation:'horizontal'
+    FloatLayout:
+        canvas.before: 
+            Color: 
+                rgb: 1, 1, 1 , 1
+            Rectangle: 
+                pos: self.pos 
+                size: self.size
+                source: 'DAACTUALBG.png'
+    BoxLayout:
+        size: min(root.size), min(root.size)
+        size_hint: None, None
+        pos_hint: {'center_x': 0.675, 'center_y': 0.525}
+        orientation: 'vertical'
+        Label:
+        Button:
+            text: 'Start Game'
+            font_size: 40
+            background_color: 1, 0, 0 , 1
+            on_press: print(1)
+            size_hint: .4, .5
+            halign: 'center'
+            valign: 'center'
+        Button:
+            text: 'High Scores'
+            font_size: 40
+            background_color: 1, 0, 0 , 1
+            on_press: root.manager.current = 'highscores'
+            size_hint: .4, .5
+        Button:
+            text: 'Settings'
+            font_size: 40
+            background_color: 1, 0, 0 , 1
+            on_press: root.manager.current = 'settings'
+            size_hint: .4, .5
+        Button:
+            text: 'Play Call Me Maybe'
+            font_size: 40
+            background_color: 1, 0, 0 , 1
+            size_hint: .4, .5
+        Label:
+<Settings>:
     BoxLayout:
         canvas.before: 
             Color: 
                 rgb: 1, 1, 1 , 1
             Rectangle: 
                 pos: self.pos 
-                size: self.size 
+                size: self.size
+                source: 'DAACTUALBG.png'
         orientation: 'vertical'
-        Image:
-            size: 24, 24
-            source: 'sad.png'
+        Label:
         Button:
-            text: 'Start Game'
+            text: 'Back to Main Menu'
             font_size: 40
             background_color: 1, 0, 0 , 1
+            on_press: root.manager.current = 'menu'
+            size_hint: .3, .1
+<HighScores>:
+    BoxLayout:
+        canvas.before: 
+            Color: 
+                rgb: 1, 1, 1 , 1
+            Rectangle: 
+                pos: self.pos 
+                size: self.size
+                source: 'DAACTUALBG.png'
+        orientation: 'vertical'
+        Label:
         Button:
-            text: 'High Scores'
+            text: 'Back to Main Menu'
             font_size: 40
             background_color: 1, 0, 0 , 1
-        Button:
-            text: 'Settings'
-            font_size: 40
-            background_color: 1, 0, 0 , 1
-        Button:
-            text: 'Play Call Me Maybe'
-            font_size: 40
-            background_color: 1, 0, 0 , 1
+            on_press: root.manager.current = 'menu'
+            size_hint: .3, .1
 ''')
+
+
+class Menu(Screen):
+    pass
+
+
+class Settings(Screen):
+    pass
+
+
+class HighScores(Screen):
+    pass
+
+
+sm = ScreenManager()
+sm.add_widget(Menu(name='menu'))
+sm.add_widget(Settings(name='settings'))
+sm.add_widget(HighScores(name='highscores'))
+
+
+
+
 
 
 class Menu(GridLayout):
@@ -54,13 +113,15 @@ class Menu(GridLayout):
     def __init__(self, **kwargs):
 
         super(Menu, self).__init__(**kwargs)
-        self.cols = 1
+        self.cols = 3
 
 
 class MyApp(App):
 
     def build(self):
-        return Menu()
+
+        self.title = 'Disciples'
+        return sm
 
 
 if __name__ == '__main__':
